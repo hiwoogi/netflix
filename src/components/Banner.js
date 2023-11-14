f530d96d5f001662ea4eaeca94ccaf6ff77b230e
hiwoogi <rmcprpwjd001@gmail.com>
2023-11-14 17:43:42 +0900

    fdsgfdgdfg

---
+++ src/components/Banner.js
@@ -0,0 +1,22 @@
+import axios from '../api/axios'
+import React, { useEffect, useState } from 'react'
+import requests from "../api/requests"
+export default function Banner() {
+  
+
+const[movie, setMovie] = useState([])
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
+  const fetchData = async() => {
+
+    const request = await axios.get(requests.fetchNowPlaying)
+    console.log(request)
+  }
+  
+    return (
+    <div>Banner</div>
+  )
+}
