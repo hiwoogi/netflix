5fb0e2a706ade517c8230479543596394daaaba9
hiwoogi <rmcprpwjd001@gmail.com>
2023-11-15 17:43:44 +0900

    fsdfdsf

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,22 +1,85 @@
-import axios from '../api/axios'
-import React, { useEffect, useState } from 'react'
-import requests from "../api/requests"
+import axios from "../api/axios";
+import React, { useEffect, useState } from "react";
+import requests from "../api/requests";
+import "./Banner.css";
+import styled from "styled-components";
 export default function Banner() {
-  
-
-    const[movie, setMovie] = useState([])
+  const [movie, setMovie] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
-    fetchData()
-  }, [])
-  
-  const fetchData = async() => {
+    fetchData();
+  }, []);
 
-    const request = await axios.get(requests.fetchNowPlaying)
-    console.log(request)
-  }
+  const fetchData = async () => {
+    const request = await axios.get(requests.fetchNowPlaying);
+
+    const movieId =
+      request.data.results[
+        Math.floor(Math.random() * request.data.results.length)
+      ].id;
+
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response: "videos" },
+    });
+
+    setMovie(movieDetail);
+  };
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
+
+  const Container = styled.div`
+    display: flex;
+    justify-content = center;
+    align-items: center;
+    flex-direction: column;
+    width:100%;
+    height:100%
+  `;
+
+  const HomeContainer = styled.div`
   
+  width:100%;
+  height: 100%;
+  
+  `;
+
+  if (!isClicked) {
     return (
-    <div>Banner</div>
-  )
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+          backgroundPosition: "top center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="banner__contents">
+          <h1>{movie.title || movie.name || movie.original_name}</h1>
+
+          <div className="banner__buttons">
+            <button
+              className="banner__button play"
+              onClick={() => setIsClicked(true)}
+            >
+              Play
+            </button>
+            <button className="banner__button info">More Information</button>
+          </div>
+
+          <h1 className="banner__description">
+            {truncate(movie.overview, 100)}
+          </h1>
+        </div>
+        <div className="banner--fadeBottom"></div>
+      </header>
+    );
+  } else {
+    return (
+      <Container>
+        <HomeContainer></HomeContainer>
+      </Container>
+    );
+  }
 }
